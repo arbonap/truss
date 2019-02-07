075ca8bfb4a077562308f26c99cfcc7dc7c60c77
Patricia Arbona <arbonap@gmail.com>
2019-02-06 23:22:35 -0500

    scrub broken utf8 csv sample and output formatted datetime in EST

--- lib/truss/parser.rb
+++ lib/truss/parser.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 require 'csv'
 require "active_support/all"
 require 'active_support/time_with_zone'
@@ -9,23 +8,20 @@ module Parser
     # this method shells it out
     `artii Truss Parser`
   end
+
   def self.import
-    output =
-    csv = CSV.foreach("sample.csv", headers: true, :encoding => 'utf-8') do |row|
-      Time.zone = 'Pacific Time (US & Canada)'
-      formatted_datetime = DateTime.strptime(row[0], '%m/%d/%y %l:%M:%S %p').in_time_zone('Pacific Time (US & Canada)')
-      formatted_datetime_est = formatted_datetime.in_time_zone('Eastern Time (US & Canada)').iso8601
-      row[0] = formatted_datetime_est
-      puts row.inspect
+    cleaned_arrays = CSV.parse(File.read("sample-with-broken-utf8.csv").scrub)
+
+    CSV.open("scrubbed-sample.csv", "a+") do |csv|
+      cleaned_arrays.map { |ary| csv << ary }
     end
-  end
-  # options = {col_sep: ";",
-  #             row_sep: "\n",
-  #             encoding: Encoding::UTF_8
-  #            }
-  def self.to_csv(fields = column_names, options = {})
-    CSV.generate(options) do |csv|
-      csv << fields
+
+    csv = CSV.foreach("scrubbed-sample.csv", headers: true, encoding: "utf-8") do |row|
+      Time.zone = 'Pacific Time (US & Canada)'
+      formatted_datetime = DateTime.strptime(row['Timestamp'], '%m/%d/%y %l:%M:%S %p').in_time_zone('Pacific Time (US & Canada)')
+      formatted_datetime_est = formatted_datetime.in_time_zone('Eastern Time (US & Canada)').iso8601
+      row['Timestamp'] = formatted_datetime_est
+      puts row.inspect
     end
   end
 end
