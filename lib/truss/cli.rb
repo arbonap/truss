4adfd76de5fdb88f4a91c264c5773712847426f3
Patricia Arbona <arbonap@gmail.com>
2019-03-07 16:00:27 -0500

    refactored script into much smaller helper methods

--- lib/truss/cli.rb
+++ lib/truss/cli.rb
@@ -3,7 +3,11 @@ require_relative "parser"
   class Cli
     extend Parser
     puts Parser.welcome
-    puts 'hi!'
+    puts 'Hello! Welcome to the Truss Parser that normalizes CSV data.
+          A normalized CSV formatted file will be outputted on `stdout`.
+          You will also see the normalized output as a CSV in `normalized_data.csv`.
+          A warning will be piped to `stderr` if there is any unparseable data,
+          and its correspnding row will be dropped from your output.'
     Parser.truncate if File.open('normalized_data.csv', "a+").present?
     Parser.import
   end
--- lib/truss/parser.rb
+++ lib/truss/parser.rb
@@ -18,76 +18,99 @@ module Parser
     # and scrub the broken bytes
     cleaned_arrays = self.cleaned_file
     # generate a new CSV without broken unicode
-    CSV.open("scrubbed-sample.csv", "w+") do |csv|
-      cleaned_arrays.map { |ary| csv << ary }
-    end
+    self.generate_scrubbed_csv(cleaned_arrays)
 
     table = CSV.table("scrubbed-sample.csv")
-    self.calculate_durations(table)
+    self.drop_unparseable_time(table)
 
     output = table.to_a.reject! { |row| row.blank? }
-
-    CSV.open("scrubbed-sample.csv", "w+") do |csv|
-      output.map { |ary| csv << ary }
-    end
+    self.generate_scrubbed_csv(output)
 
     CSV.foreach("scrubbed-sample.csv", headers: true, encoding: "utf-8") do |row|
       # convert PST to EST
       # format timestamps in iso8601
-      Time.zone = 'Pacific Time (US & Canada)'
-      formatted_datetime = DateTime.strptime(row['timestamp'], '%m/%d/%y %l:%M:%S %p').in_time_zone('Pacific Time (US & Canada)')
-      formatted_datetime_est = formatted_datetime.in_time_zone('Eastern Time (US & Canada)').iso8601
-      row['timestamp'] = formatted_datetime_est
+      self.update_timezone(row, 'Pacific Time (US & Canada)', 'Eastern Time (US & Canada)')
 
       # any zip codes with less than 5 digits,
       # prepend 0's to them until they are 5 digits long
-       until row['zip'].length == 5 do
-         row['zip'].prepend('0')
-       end
+       self.validate_zipcode(row['zip'])
        # uppercase all names
-       row['fullname'].upcase!
+       self.upcase_fullname(row['fullname'])
 
        # pass address column as is, validate everything is valid unicode
        # else, replace with Unicode Replacement Character
-      #row['Address'].force_encoding('UTF-8').encode('UTF-16', :invalid => :replace, :replace => '�').encode('UTF-8')
-
        self.validate_address(row['address'])
 
-       foo_duration_seconds = Time.strptime(row['fooduration'], '%H:%M:%S.%L').seconds_since_midnight.to_f
-       row['fooduration'] = foo_duration_seconds
-
-       bar_duration_seconds = Time.strptime(row['barduration'], '%H:%M:%S.%L').seconds_since_midnight.to_f
-       row['barduration'] = bar_duration_seconds
+       foo_duration_seconds = self.calculate_duration(row['fooduration'])
+       bar_duration_seconds = self.calculate_duration(row['barduration'])
 
        row['totalduration'] = foo_duration_seconds + bar_duration_seconds
 
-       row['address'] = '' if row['address'].nil?
-       row['notes'] = '' if row['notes'].nil?
-
-       row['notes'].encode('UTF-16', :undef => :replace, :invalid => :replace, :replace => '�').encode('UTF-8')
+      self.unicode_notes_validation(row['notes'])
 
       CSV.open('normalized_data.csv', 'a') do |csv|
         csv << row.fields
       end
     end
-    File.open('normalized_data.csv').map { |r| puts r}
+    File.open('normalized_data.csv').map { |row| puts row }
   end
 
   def self.truncate
     File.truncate('normalized_data.csv', 0)
   end
 
+  def self.generate_scrubbed_csv(arrays)
+    CSV.open("scrubbed-sample.csv", "w+") do |csv|
+      arrays.map { |ary| csv << ary }
+    end
+  end
+
   def self.cleaned_file
     CSV.parse(File.read("#{ARGV[0]}").scrub)
   end
 
-  def self.calculate_durations(table)
+  def self.unicode_notes_validation(notes)
+    notes = '' if notes.nil?
+    notes.encode('UTF-16', :undef => :replace, :invalid => :replace, :replace => '�').encode('UTF-8')
+  end
+
+  def self.upcase_fullname(fullname)
+    fullname.upcase!
+  end
+
+  def self.validate_zipcode(zipcode)
+    until zipcode.length == 5 do
+      zipcode.prepend('0')
+    end
+  end
+
+  def self.format_timestamp(timestamp)
+    DateTime.strptime(timestamp, '%m/%d/%y %l:%M:%S %p')
+  end
+
+  def self.update_timezone(row, beginning_tz, result_tz)
+    Time.zone = beginning_tz
+    datetime = self.format_timestamp(row['timestamp']).in_time_zone(beginning_tz)
+    datetime_est = datetime.in_time_zone(result_tz)
+    formatted_datetime_est = self.format_datetime(datetime_est)
+    row['timestamp'] = formatted_datetime_est
+  end
+
+  def self.format_datetime(timestamp)
+    timestamp.iso8601
+  end
+
+  def self.calculate_duration(row)
+    duration_seconds = Time.strptime(row, '%H:%M:%S.%L').seconds_since_midnight.to_f
+    row = duration_seconds
+  end
+
+  def self.drop_unparseable_time(table)
     table.each_with_index do |row, i|
       begin
-        # Time.zone = 'Pacific Time (US & Canada)'
-        # formatted_datetime = DateTime.strptime(row['timestamp'], '%m/%d/%y %l:%M:%S %p').in_time_zone('Pacific Time (US & Canada)')
-        Time.strptime(row[:fooduration], '%H:%M:%S.%L').seconds_since_midnight.to_f
-        Time.strptime(row[:barduration], '%H:%M:%S.%L').seconds_since_midnight.to_f
+        self.calculate_duration(row[:fooduration])
+        self.calculate_duration(row[:barduration])
+        self.format_timestamp(row[:timestamp])
       rescue ArgumentError => e
         STDERR.puts "Warning: Row #{i} will be deleted due to an unparseable Time.
                     The following row of data will be dropped:
@@ -98,6 +121,7 @@ module Parser
   end
 
   def self.validate_address(address)
+    address = '' if address.nil?
     address.encode('UTF-16', :invalid => :replace, :replace => '�').encode('UTF-8')
   end
 end
